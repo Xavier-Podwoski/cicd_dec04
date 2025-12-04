cc7696729fdeaed6629b9c7cf72d1fa83ec21c5a
Xavier-Podwoski <xpodwoski@gmail.com>
2025-12-04 18:03:15 +0000

    CICD Tests

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from src.app import add
+
+import sys
+from pathlib import Path
+
+root = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(root / "src"))
+
+def test_add():
+    assert add(5, 6) == 11
+
+def test_add2():
+    assert add (5, 6) != 10
+
+def test_add3():
+    assert add (5, 6) != 13
